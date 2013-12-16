40f5d8b8ffa243f426377438745219c249e96018
fahad.javaid <fahad.javaid@rackspace.co.uk>
2013-12-16 11:14:54 +0000

    first commit

---
+++ cloudfiles.py
@@ -0,0 +1,50 @@
+import pyrax
+pyrax.set_default_region("LON")
+pyrax.set_setting("identity_type", "rackspace")
+pyrax.set_credentials("davecottrell1", "799723e291376136b2b5a9cb7e0779ad")
+
+
+
+"""cf stads for cloud files, pyrax cloud files"""
+cf = pyrax.cloudfiles 
+
+""" Create a Container """
+
+print "This is the List of Existing Containers under your Account"
+print cf.list_containers()
+print cf.list_containers_info()
+
+
+#name_of_container = raw_input("Please Enter the name for new container : ")
+#cf.create_container(name_of_container)
+
+
+""" List all containers """
+
+
+
+
+ 
+
+""" Create Container """
+
+#name_of_container = raw_input("Please Enter the name: ")
+
+#cont = cf.create_container(name_of_container)
+
+cont = cf.get_container("Fahad")
+#print "Container: ", cont
+
+
+"""Deleting the objects in the Container"""
+
+#container_name = "Fahad"
+#file_name = "IIS7NOTES"
+
+
+#pyrax.cloudfiles.delete_container("Fahad")
+
+
+    
+cf.user_agent
+
