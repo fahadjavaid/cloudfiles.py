05b0d623a2337e29e5774923401d948a6c779c78
fahadjavaid <fahad.javaid@rackspace.co.uk>
2013-12-18 09:58:10 +0000

    Update cloudserver.py

--- cloudserver.py
+++ cloudserver.py
@@ -1,7 +1,6 @@
-
 import pyrax
 pyrax.set_default_region("LON")
-pyrax.set_credentials("davecottrell1", "799723e291376136b2b5a9cb7e0779ad")
+pyrax.set_credentials("username", "your_apikey")
 
 """ List of servers in the account """
 
