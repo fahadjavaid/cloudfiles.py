a73a8b295f7830e891d65fc6e17d458c3479dbaa
Fahad Javaid <fahad.javaid@rackspace.co.uk>
2014-09-11 15:26:11 +0100

    Update all-service.ps1

--- all-service.ps1
+++ all-service.ps1
@@ -1,3 +1,8 @@
+#Script Author: Fahad Javaid <fahad.javaid@rackspace.co.uk>
+#Contribution/Mentor: Mark Wichall
+
+Function Uptime {
+
 $computer = $env:computername
 $lastboot = (Get-WmiObject -Class Win32_OperatingSystem).LastBootUpTime
 $lastboottime = [System.Management.ManagementDateTimeconverter]::ToDateTime($lastboot)
@@ -7,17 +12,13 @@ $boot = (get-date) - [System.Management.ManagementDateTimeconverter]::ToDateTime
 Write-Host -fore Green  "Checking for Server Uptime Details: "
 ""
 write-host "Servername" $computer;write-host "Current Date & Time = " (get-date);Write-Host "Last Boot Date & Time = " $lastboottime
-Write-Host "Server has been up for: " $boot.days "days" $boot.hours "hours" $boot.minutes  "minutes";
+Write-Host "Server has been up for: " $boot.days "days" $boot.hours "hours" $boot.minutes  "minutes`n";
 
-function All-service {
+}
 
-Write-Host -fore Yellow "Checking System Logs:"
+function All-service ($events) {
 
-
-
-$events = get-eventlog -logname system |where-object {$_.eventid -eq 1074 -or $_.eventid -eq "6008" -or $_.eventid -eq "41"}
-
-$Userrestart = $events | where-object {$_.eventid -eq 1074} | select-object Timewritten, UserName -first 3 | ft -autosize
+$Userrestart = $events | where-object {$_.eventid -eq 1074} | select-object Timewritten, UserName -first 5 | ft -autosize
 
 $unexpectedshutdown = $events | where-object {$_.eventid -eq "6008" -or $_.eventid -eq "41" } |select-object message, Timewritten -first 3 | ft -autosize
 
@@ -25,23 +26,59 @@ $bluescreen = Get-EventLog -LogName application -Newest 100 -Source 'Windows Err
 select timewritten, message | where message -match 'bluescreen' |  ft -auto -wrap
 
 if (!$Userrestart) 
-{write-host -fore Green "No matching system log events found for user initiated restart..."}
-else {Write-host -Fore Green "Last three User restarts"; $userrestart}
+{write-host -fore Green "No matching system log events found for user initiated restart...`n"}
+else {Write-host -Fore Green "Last 5 User restarts`n"; $userrestart}
 
 if(!$unexpectedshutdown)
-{Write-Host -fore Green "No matching system log events found for unexpected shutdown..."}
-else {Write-Host -fore Red "Last three unexpected shutdowns"; $unexpectedshutdown}
-}
+{Write-Host -fore Green "No matching system log events found for unexpected shutdown...`n"}
+else {Write-Host -fore Red "Last three unexpected shutdowns`n"; $unexpectedshutdown}
 
 if(!$bluescreen)
-{Write-host -fore Green "No matching logs found for BSOD...."}
-else {Write-Host -fore Red "BSOD details"; $bluescreen}
-
-function windows-update {
-
-$windows_update = Get-hotfix |select-object description, installedon, hotfixid -last 5 |ft
-$windows_update
+{Write-host -fore Green "No matching logs found for BSOD....`n"}
+else {Write-Host -fore Red "BSOD details`n"; $bluescreen}
 }
 
-All-service
-windows-update
+Function Using-Culture (
+    [System.Globalization.CultureInfo]$culture = (throw "USAGE: Using-Culture -Culture culture -Script {scriptblock}"),
+    [ScriptBlock]$script= (throw "USAGE: Using-Culture -Culture culture -Script {scriptblock}"))
+{
+    $OldCulture = [System.Threading.Thread]::CurrentThread.CurrentCulture
+    trap
+    {
+        [System.Threading.Thread]::CurrentThread.CurrentCulture = $OldCulture
+    }
+    [System.Threading.Thread]::CurrentThread.CurrentCulture = $culture
+    Invoke-Command $script
+    [System.Threading.Thread]::CurrentThread.CurrentCulture = $OldCulture
+}
+
+#Author: Rodney Baulch <rodney.baulch@rackspace.co.uk>
+Function Using-Culture (
+    [System.Globalization.CultureInfo]$culture = (throw "USAGE: Using-Culture -Culture culture -Script {scriptblock}"),
+    [ScriptBlock]$script= (throw "USAGE: Using-Culture -Culture culture -Script {scriptblock}"))
+{
+    $OldCulture = [System.Threading.Thread]::CurrentThread.CurrentCulture
+    trap
+    {
+        [System.Threading.Thread]::CurrentThread.CurrentCulture = $OldCulture
+    }
+    [System.Threading.Thread]::CurrentThread.CurrentCulture = $culture
+    Invoke-Command $script
+    [System.Threading.Thread]::CurrentThread.CurrentCulture = $OldCulture
+}
+
+Write-Host -fore Yellow "1/2 - Checking System Logs...Please wait`n"
+$events = get-eventlog -logname system |where-object {$_.eventid -eq 1074 -or $_.eventid -eq "6008" -or $_.eventid -eq "41"}
+
+Write-Host -fore Yellow "2/2 - Checking Installed Hotfixes...Please wait`n"
+$Hotfixes = Using-Culture en-US { Get-HotFix | Select-Object -last 20 | Sort-Object HotFixID -Descending | Format-Table Description,HotfixID,InstalledBy,@{Name="Installation Date"; Expression={"{0:dd/MM/yyyy}" -f $_.InstalledOn}} -AutoSize } 
+
+#Clear screen
+Clear
+
+#Run Functions
+Uptime
+All-service ($events)
+
+Write-Host "Last 20 hotfixes installed:`n" -ForegroundColor Green
+$Hotfixes
